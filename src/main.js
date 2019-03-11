ca091d3ebdbbdfcf320e52e9e5fcb41b922516f0
Sung Zheng Jie <sungzhengjie96@gmail.com>
2019-03-11 20:07:13 +0800

    Include mdb package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbvue/build/css/mdb.css';
 import App from "./App.vue";
 import router from "./router";
+
 
 import MaterialKit from "./plugins/material-kit";
 
