77bf0eb143bd4df93154a6bc4caa11b692cfab36
Joel <e0176286@u.nus.edu>
2019-03-12 02:16:43 +0800

    update module

--- src/views/Module.vue
+++ src/views/Module.vue
@@ -1,305 +1,307 @@
-  <template>
-    <div class="wrapper">
-    <section id="dashboard">
-      <mdb-card class="mb-4">
-      </mdb-card>
-      <div><br/></div>
-      <section class="mt-lg-5">
+    <template>
+      <div class="wrapper">
+      <section id="dashboard">
+        <div><br/></div>
         
-          <div class="col justify-content-center">
+        <section class="mt-lg-5">
+          
+            <div class="col justify-content-center">
 
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">BT1101: Introduction to Business Analytics</h5>
-              </div><br/>
-            <nav-tabs-card no-label>
-              <template slot="content">
-                  <md-tabs md-sync-route class="md-primary" md-alignment="justify-content-center">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">BT1101: Introduction to Business Analytics</h5>
+                </div>
 
-                  <md-tab id="tab-grade" md-label="Grade Distribution" md-icon="face">
-                  </md-tab>
+  <div class="row justify-content-center" >
+  <div class="w-50 p-3" style="background-color: #eee;"><nav-tabs-card no-label>
+                <template slot="content">
+                    <md-tabs md-sync-route class="md-warning" md-alignment="justify-content-center">
 
-                  <md-tab id="tab-faulty" md-label="Faculty Distribution" md-icon="chat">
-                  </md-tab>
+                    <md-tab id="tab-grade" md-label="Grade Distribution" md-icon="face">
+                    </md-tab>
 
-                  <md-tab id="tab-student" md-label="Student Feedback" md-icon="build">
-                  </md-tab>
+                    <md-tab id="tab-faulty" md-label="Faculty Distribution" md-icon="chat">
+                    </md-tab>
 
-                  <md-tab id="tab-teaching" md-label="Teaching Feedback" md-icon="build">
-                  </md-tab>
+                    <md-tab id="tab-student" md-label="Student Feedback" md-icon="build">
+                    </md-tab>
 
-                </md-tabs>
+                    <md-tab id="tab-teaching" md-label="Teaching Feedback" md-icon="build">
+                    </md-tab>
 
-                <mdb-card-body>
-                        <div style="display: block">
-                          <mdb-bar-chart :data="barChartData" :options="barChartOptions" :height="500" :width="300"/>
-                        </div>
-                </mdb-card-body>
+                  </md-tabs>
 
-              </template>
-    </nav-tabs-card>
+                  <mdb-card-body>
+                          <div style="display: block">
+                            <mdb-bar-chart :data="barChartData" :options="barChartOptions" :height="500" :width="300"/>
+                          </div>
+                      </mdb-card-body>
 
-        </div>
+                </template>
+      </nav-tabs-card></div>
+
+          </div>
+  </div>
+        </section>
       </section>
-    </section>
-    </div>
-  </template>
+      </div>
+    </template>
 
-<script>
-import { mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardBody, mdbCardHeader, mdbCardText, mdbIcon, mdbTbl, mdbBarChart, mdbPieChart, mdbLineChart, mdbRadarChart, mdbDoughnutChart, mdbListGroup, mdbListGroupItem, mdbBadge, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
+  <script>
+  import { mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardBody, mdbCardHeader, mdbCardText, mdbIcon, mdbTbl, mdbBarChart, mdbPieChart, mdbLineChart, mdbRadarChart, mdbDoughnutChart, mdbListGroup, mdbListGroupItem, mdbBadge, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
 
-import {
-  NavTabsCard
-} from '@/components'
+  import {
+    NavTabsCard
+  } from '@/components'
 
-export default {
-  name: 'Dashboard',
-  bodyClass: "landing-page",
-  props: {
-    header: {
-      type: String,
-      default: require("@/assets/img/bg7.jpg")
-    }
-  },
-  computed: {
-    headerStyle() {
+  export default {
+    name: 'Dashboard',
+    bodyClass: "landing-page",
+    props: {
+      header: {
+        type: String,
+        default: require("@/assets/img/bg7.jpg")
+      }
+    },
+    computed: {
+      headerStyle() {
+        return {
+          backgroundImage: `url(${this.header})`
+        };
+      }
+    },
+    components: {
+      NavTabsCard,
+      mdbRow,
+      mdbCol,
+      mdbBtn,
+      mdbCard,
+      mdbCardBody,
+      mdbCardHeader,
+      mdbCardText,
+      mdbIcon,
+      mdbTbl,
+      mdbBarChart,
+      mdbPieChart,
+      mdbLineChart,
+      mdbRadarChart,
+      mdbDoughnutChart,
+      mdbListGroup,
+      mdbListGroupItem,
+      mdbBadge,
+      mdbModal,
+      mdbModalHeader,
+      mdbModalTitle,
+      mdbModalBody,
+      mdbModalFooter
+    },
+    data () {
       return {
-        backgroundImage: `url(${this.header})`
-      };
-    }
-  },
-  components: {
-    NavTabsCard,
-    mdbRow,
-    mdbCol,
-    mdbBtn,
-    mdbCard,
-    mdbCardBody,
-    mdbCardHeader,
-    mdbCardText,
-    mdbIcon,
-    mdbTbl,
-    mdbBarChart,
-    mdbPieChart,
-    mdbLineChart,
-    mdbRadarChart,
-    mdbDoughnutChart,
-    mdbListGroup,
-    mdbListGroupItem,
-    mdbBadge,
-    mdbModal,
-    mdbModalHeader,
-    mdbModalTitle,
-    mdbModalBody,
-    mdbModalFooter
-  },
-  data () {
-    return {
-      showFrameModalTop: false,
-      showFrameModalBottom: false,
-      showSideModalTopRight: false,
-      showSideModalTopLeft: false,
-      showSideModalBottomRight: false,
-      showSideModalBottomLeft: false,
-      showCentralModalSmall: false,
-      showCentralModalMedium: false,
-      showCentralModalLarge: false,
-      showCentralModalFluid: false,
-      showFluidModalRight: false,
-      showFluidModalLeft: false,
-      showFluidModalTop: false,
-      showFluidModalBottom: false,
-      barChartData: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        datasets: [
-          {
-            label: '#1',
-            data: [12, 39, 3, 50, 2, 32, 84],
-            backgroundColor: 'rgba(245, 74, 85, 0.5)',
-            borderWidth: 1
-          }, {
-            label: '#2',
-            data: [56, 24, 5, 16, 45, 24, 8],
-            backgroundColor: 'rgba(90, 173, 246, 0.5)',
-            borderWidth: 1
-          }, {
-            label: '#3',
-            data: [12, 25, 54, 3, 15, 44, 3],
-            backgroundColor: 'rgba(245, 192, 50, 0.5)',
-            borderWidth: 1
-          }
-        ]
-      },
-      barChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [{
-            barPercentage: 1,
-            gridLines: {
-              display: true,
-              color: 'rgba(0, 0, 0, 0.1)'
+        showFrameModalTop: false,
+        showFrameModalBottom: false,
+        showSideModalTopRight: false,
+        showSideModalTopLeft: false,
+        showSideModalBottomRight: false,
+        showSideModalBottomLeft: false,
+        showCentralModalSmall: false,
+        showCentralModalMedium: false,
+        showCentralModalLarge: false,
+        showCentralModalFluid: false,
+        showFluidModalRight: false,
+        showFluidModalLeft: false,
+        showFluidModalTop: false,
+        showFluidModalBottom: false,
+        barChartData: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+          datasets: [
+            {
+              label: '#1',
+              data: [12, 39, 3, 50, 2, 32, 84],
+              backgroundColor: 'rgba(245, 74, 85, 0.5)',
+              borderWidth: 1
+            }, {
+              label: '#2',
+              data: [56, 24, 5, 16, 45, 24, 8],
+              backgroundColor: 'rgba(90, 173, 246, 0.5)',
+              borderWidth: 1
+            }, {
+              label: '#3',
+              data: [12, 25, 54, 3, 15, 44, 3],
+              backgroundColor: 'rgba(245, 192, 50, 0.5)',
+              borderWidth: 1
             }
-          }],
-          yAxes: [{
-            gridLines: {
-              display: true,
-              color: 'rgba(0, 0, 0, 0.1)'
+          ]
+        },
+        barChartOptions: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [{
+              barPercentage: 1,
+              gridLines: {
+                display: true,
+                color: 'rgba(0, 0, 0, 0.1)'
+              }
+            }],
+            yAxes: [{
+              gridLines: {
+                display: true,
+                color: 'rgba(0, 0, 0, 0.1)'
+              },
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        },
+        pieChartData: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+          datasets: [
+            {
+              data: [300, 50, 100, 40, 120, 24, 52],
+              backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#ac64ad'],
+              hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#da92db']
+            }
+          ]
+        },
+        pieChartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
+        },
+        lineChartData: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+          datasets: [
+            {
+              label: '#1',
+              backgroundColor: 'rgba(245, 74, 85, 0.5)',
+              data: [65, 59, 80, 81, 56, 55, 40]
             },
-            ticks: {
-              beginAtZero: true
+            {
+              label: '#2',
+              backgroundColor: 'rgba(90, 173, 246, 0.5)',
+              data: [12, 42, 121, 56, 24, 12, 2]
+            },
+            {
+              label: '#3',
+              backgroundColor: 'rgba(245, 192, 50, 0.5)',
+              data: [2, 123, 154, 76, 54, 23, 5]
             }
-          }]
+          ]
+        },
+        lineChartOptions: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [{
+              gridLines: {
+                display: true,
+                color: 'rgba(0, 0, 0, 0.1)'
+              }
+            }],
+            yAxes: [{
+              gridLines: {
+                display: true,
+                color: 'rgba(0, 0, 0, 0.1)'
+              }
+            }]
+          }
+        },
+        radarChartData: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+          datasets: [
+            {
+              label: '#1',
+              backgroundColor: 'rgba(245, 74, 85, 0.5)',
+              data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+              label: '#2',
+              backgroundColor: 'rgba(90, 173, 246, 0.5)',
+              data: [12, 42, 121, 56, 24, 12, 2]
+            },
+            {
+              label: '#3',
+              backgroundColor: 'rgba(245, 192, 50, 0.5)',
+              data: [2, 123, 154, 76, 54, 23, 5]
+            }
+          ]
+        },
+        radarChartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
+        },
+        doughnutChartData: {
+          labels: ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'],
+          datasets: [
+            {
+              data: [300, 50, 100, 40, 120],
+              backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+              hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774']
+            }
+          ]
+        },
+        doughnutChartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
         }
-      },
-      pieChartData: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        datasets: [
-          {
-            data: [300, 50, 100, 40, 120, 24, 52],
-            backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#ac64ad'],
-            hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#da92db']
-          }
-        ]
-      },
-      pieChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
-      },
-      lineChartData: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        datasets: [
-          {
-            label: '#1',
-            backgroundColor: 'rgba(245, 74, 85, 0.5)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: '#2',
-            backgroundColor: 'rgba(90, 173, 246, 0.5)',
-            data: [12, 42, 121, 56, 24, 12, 2]
-          },
-          {
-            label: '#3',
-            backgroundColor: 'rgba(245, 192, 50, 0.5)',
-            data: [2, 123, 154, 76, 54, 23, 5]
-          }
-        ]
-      },
-      lineChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: true,
-              color: 'rgba(0, 0, 0, 0.1)'
-            }
-          }],
-          yAxes: [{
-            gridLines: {
-              display: true,
-              color: 'rgba(0, 0, 0, 0.1)'
-            }
-          }]
-        }
-      },
-      radarChartData: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-        datasets: [
-          {
-            label: '#1',
-            backgroundColor: 'rgba(245, 74, 85, 0.5)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: '#2',
-            backgroundColor: 'rgba(90, 173, 246, 0.5)',
-            data: [12, 42, 121, 56, 24, 12, 2]
-          },
-          {
-            label: '#3',
-            backgroundColor: 'rgba(245, 192, 50, 0.5)',
-            data: [2, 123, 154, 76, 54, 23, 5]
-          }
-        ]
-      },
-      radarChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
-      },
-      doughnutChartData: {
-        labels: ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'],
-        datasets: [
-          {
-            data: [300, 50, 100, 40, 120],
-            backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
-            hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774']
-          }
-        ]
-      },
-      doughnutChartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
       }
     }
   }
-}
 
 
 
-</script>
+  </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.cascading-admin-card {
-  margin: 100px 0;
-}
-.cascading-admin-card .admin-up {
-  margin-left: 4%;
-  margin-right: 4%;
-  margin-top: -20px;
-}
-.cascading-admin-card .admin-up .fas,
-.cascading-admin-card .admin-up .far {
-  box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.2), 0 2px 13px 0 rgba(0, 0, 0, 0.19);
-  padding: 1.7rem;
-  font-size: 2rem;
-  color: #fff;
-  text-align: left;
-  margin-right: 1rem;
-  border-radius: 3px;
-}
-.cascading-admin-card .admin-up .data {
-  float: right;
-  margin-top: 2rem;
-  text-align: right;
-}
-.admin-up .data p {
-  color: #999999;
-  font-size: 12px;
-}
-.classic-admin-card .card-body {
-  color: #fff;
-  margin-bottom: 0;
-  padding: 0.9rem;
-}
-.classic-admin-card .card-body p {
-  font-size: 13px;
-  opacity: 0.7;
-  margin-bottom: 0;
-}
-.classic-admin-card .card-body h4 {
-  margin-top: 10px;
-}
-.md-card-actions.text-center {
-  display: flex;
-  justify-content: center !important;
-}
-.contact-form {
-  margin-top: 30px;
-}
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  .cascading-admin-card {
+    margin: 100px 0;
+  }
+  .cascading-admin-card .admin-up {
+    margin-left: 4%;
+    margin-right: 4%;
+    margin-top: -20px;
+  }
+  .cascading-admin-card .admin-up .fas,
+  .cascading-admin-card .admin-up .far {
+    box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.2), 0 2px 13px 0 rgba(0, 0, 0, 0.19);
+    padding: 1.7rem;
+    font-size: 2rem;
+    color: #fff;
+    text-align: left;
+    margin-right: 1rem;
+    border-radius: 3px;
+  }
+  .cascading-admin-card .admin-up .data {
+    float: right;
+    margin-top: 2rem;
+    text-align: right;
+  }
+  .admin-up .data p {
+    color: #999999;
+    font-size: 12px;
+  }
+  .classic-admin-card .card-body {
+    color: #fff;
+    margin-bottom: 0;
+    padding: 0.9rem;
+  }
+  .classic-admin-card .card-body p {
+    font-size: 13px;
+    opacity: 0.7;
+    margin-bottom: 0;
+  }
+  .classic-admin-card .card-body h4 {
+    margin-top: 10px;
+  }
+  .md-card-actions.text-center {
+    display: flex;
+    justify-content: center !important;
+  }
+  .contact-form {
+    margin-top: 30px;
+  }
 
-.md-has-textarea + .md-layout {
-  margin-top: 15px;
-}
-</style>
+  .md-has-textarea + .md-layout {
+    margin-top: 15px;
+  }
+  </style>
