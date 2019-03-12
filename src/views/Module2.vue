e216be45b00f3cf2ad69a9f5db65e09bf15e835e
DESKTOP-0NM1NV5\User <e0202290@u.nus.edu>
2019-03-13 01:18:46 +0800

    everything

---
+++ src/views/Module2.vue
@@ -0,0 +1,369 @@
+<template>
+   <div class="wrapper">
+      <section id="dashboard">
+        <mdb-container>
+          <mdb-row>
+            <mdb-col> 
+            </mdb-col>
+            <mdb-col col = "3">
+         <div><br/><br/></div>
+            <mdb-row md="4" center>
+              <mdb-modal-title>
+                <div><br/><br/></div>
+                <mdb-row md="4" center>
+                  <h1 class="card-title" style="color:#03A9F4;">BT1101</h1>
+                  </mdb-row>
+                      <mdb-modal-footer style="color:#03A9F4;" center>
+                        <mdb-col center style="color:#03A9F4;">
+                        Introduction to Business Analytics
+                        <mdb-row md="4" center>
+                      <h5 style="color:#03A9F4;">
+                       <mdb-icon icon="fas fa-star" size="2x"/>
+                       <mdb-icon icon="fas fa-star" size="2x"/>
+                       <mdb-icon icon="fas fa-star" size="2x"/>
+                       <mdb-icon icon="fas fa-star" size="2x"/>
+                       <mdb-icon icon="far fa-star" size="2x"/>
+                     </h5>
+                     </mdb-row>
+                   </mdb-col>
+                      </mdb-modal-footer>
+                      </mdb-modal-title>
+
+                     </mdb-row>
+     <mdb-row md="4" center>
+      <div class="card card-cascade narrower">
+    <div class="card-body card-body-cascade text-left">
+        <div style="display: block" justify-content-center>
+            <h4 style="color:#03A9F4;">
+This module provides students with an introduction to the fundamental concepts and tools needed to understand the emerging role of business analytics in business and non-profit organizations. The module aims to demonstrate to students how to apply basic business analytics tools in a spreadsheet environment, and how to communicate with analytics professionals to effectively use and interpret analytic models and results for making better and more well-informed business decisions.
+            </h4>
+          </div>
+      </div>
+      </div>
+            </mdb-row>
+            </mdb-col>
+            <span class="border-right border-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <mdb-col col = "8">
+         <section class="mt-lg-5">
+            <mdb-col center>
+                     <mdb-row md="4" center>
+                        <mdb-modal-title>
+                            <div><br/><br/><br/></div>
+                           <mdb-modal size v-if="modal" @close="modal = false">
+                                <mdb-modal-header>
+                                    <mdb-modal-title>Module Information</mdb-modal-title>
+                                </mdb-modal-header>
+                                <mdb-modal-body><p>This module provides students with an introduction to the fundamental concepts and tools needed to understand the emerging role of business analytics in business and non-profit organizations. The module aims to demonstrate to students how to apply basic business analytics tools in a spreadsheet environment, and how to communicate with analytics professionals to effectively use and interpret analytic models and results for making better and more well-informed business decisions.</p></mdb-modal-body>
+                                <mdb-modal-footer>
+                                    <mdb-btn color="secondary" size="sm" @click.native="modal = false">Close</mdb-btn>
+                                    <mdb-btn color="primary" size="sm">Save changes</mdb-btn>
+                                </mdb-modal-footer>
+                            </mdb-modal>
+
+                        </mdb-modal-title> 
+                     </mdb-row>                   
+                  </mdb-col>
+               
+               <mdb-row center>
+                  <div class="w-75 p-3" style="background-color: #eee;">
+                     <nav-tabs-card no-label>
+                        <template slot="content">
+                           <mdb-row center>
+                              <md-tabs md-sync-route class="md-warning" md-alignment="justify-content-center">
+                                 <md-tab id="tab-grade" md-label="Grade Distribution" md-icon="face">
+                                 </md-tab>
+                                 <md-tab id="tab-faulty" md-label="Faculty Distribution" md-icon="chat">
+                                 </md-tab>
+                                 <md-tab id="tab-student" md-label="Student Feedback" md-icon="build">
+                                 </md-tab>
+                                 <md-tab id="tab-teaching" md-label="Teaching Feedback" md-icon="build">
+                                 </md-tab>
+                              </md-tabs>
+                           </mdb-row>
+                           <mdb-card-body>
+                              <div style="display: block" justify-content-center>
+                                 <mdb-bar-chart :data="barChartData" :options="barChartOptions" :height="500" :width="300"/>
+                              </div>
+                           </mdb-card-body>
+                        </template>
+                     </nav-tabs-card>
+                  </div>
+               </mdb-row>
+         </section>
+         </mdb-col>
+         </mdb-row>
+         </mdb-container>
+      </section>
+    </div>
+</template>
+<script>
+   import { mdbRow, mdbCol, mdbBtn, mdbCard, mdbCardBody, mdbCardHeader, mdbCardText, mdbIcon, mdbTbl, mdbBarChart, mdbPieChart, mdbLineChart, mdbRadarChart, mdbDoughnutChart, mdbListGroup, mdbListGroupItem, mdbBadge, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
+   
+   import {
+     NavTabsCard
+   } from '@/components'
+   
+   export default {
+   name: 'Dashboard',
+   bodyClass: "landing-page",
+   props: {
+   header: {
+   type: String,
+   default: require("@/assets/img/bg7.jpg")
+   }
+   },
+   computed: {
+   headerStyle() {
+   return {
+     backgroundImage: `url(${this.header})`
+   };
+   }
+   },
+   components: {
+   NavTabsCard,
+   mdbRow,
+   mdbCol,
+   mdbBtn,
+   mdbCard,
+   mdbCardBody,
+   mdbCardHeader,
+   mdbCardText,
+   mdbIcon,
+   mdbTbl,
+   mdbBarChart,
+   mdbPieChart,
+   mdbLineChart,
+   mdbRadarChart,
+   mdbDoughnutChart,
+   mdbListGroup,
+   mdbListGroupItem,
+   mdbBadge,
+   mdbModal,
+   mdbModalHeader,
+   mdbModalTitle,
+   mdbModalBody,
+   mdbModalFooter
+   },
+   data () {
+   return {
+   modal: false,
+   showFrameModalTop: false,
+   showFrameModalBottom: false,
+   showSideModalTopRight: false,
+   showSideModalTopLeft: false,
+   showSideModalBottomRight: false,
+   showSideModalBottomLeft: false,
+   showCentralModalSmall: false,
+   showCentralModalMedium: false,
+   showCentralModalLarge: false,
+   showCentralModalFluid: false,
+   showFluidModalRight: false,
+   showFluidModalLeft: false,
+   showFluidModalTop: false,
+   showFluidModalBottom: false,
+   barChartData: {
+     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+     datasets: [
+       {
+         label: '#1',
+         data: [12, 39, 3, 50, 2, 32, 84],
+         backgroundColor: 'rgba(245, 74, 85, 0.5)',
+         borderWidth: 1
+       }, {
+         label: '#2',
+         data: [56, 24, 5, 16, 45, 24, 8],
+         backgroundColor: 'rgba(90, 173, 246, 0.5)',
+         borderWidth: 1
+       }, {
+         label: '#3',
+         data: [12, 25, 54, 3, 15, 44, 3],
+         backgroundColor: 'rgba(245, 192, 50, 0.5)',
+         borderWidth: 1
+   
+             }
+           ]
+         },
+         barChartOptions: {
+           responsive: true,
+           maintainAspectRatio: false,
+           scales: {
+             xAxes: [{
+               barPercentage: 1,
+               gridLines: {
+                 display: true,
+                 color: 'rgba(0, 0, 0, 0.1)'
+               }
+             }],
+             yAxes: [{
+               gridLines: {
+                 display: true,
+                 color: 'rgba(0, 0, 0, 0.1)'
+               },
+               ticks: {
+                 beginAtZero: true
+               }
+             }]
+           }
+         },
+         pieChartData: {
+           labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+           datasets: [
+             {
+               data: [300, 50, 100, 40, 120, 24, 52],
+               backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#ac64ad'],
+               hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#da92db']
+             }
+           ]
+         },
+         pieChartOptions: {
+           responsive: true,
+           maintainAspectRatio: false
+         },
+         lineChartData: {
+           labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+           datasets: [
+             {
+               label: '#1',
+               backgroundColor: 'rgba(245, 74, 85, 0.5)',
+               data: [65, 59, 80, 81, 56, 55, 40]
+             },
+             {
+               label: '#2',
+               backgroundColor: 'rgba(90, 173, 246, 0.5)',
+               data: [12, 42, 121, 56, 24, 12, 2]
+             },
+             {
+               label: '#3',
+               backgroundColor: 'rgba(245, 192, 50, 0.5)',
+               data: [2, 123, 154, 76, 54, 23, 5]
+             }
+           ]
+         },
+         lineChartOptions: {
+           responsive: true,
+           maintainAspectRatio: false,
+           scales: {
+             xAxes: [{
+               gridLines: {
+                 display: true,
+                 color: 'rgba(0, 0, 0, 0.1)'
+               }
+             }],
+             yAxes: [{
+               gridLines: {
+                 display: true,
+                 color: 'rgba(0, 0, 0, 0.1)'
+               }
+             }]
+           }
+         },
+         radarChartData: {
+           labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+           datasets: [
+             {
+               label: '#1',
+               backgroundColor: 'rgba(245, 74, 85, 0.5)',
+               data: [65, 59, 80, 81, 56, 55, 40]
+             },
+             {
+               label: '#2',
+               backgroundColor: 'rgba(90, 173, 246, 0.5)',
+               data: [12, 42, 121, 56, 24, 12, 2]
+             },
+             {
+               label: '#3',
+               backgroundColor: 'rgba(245, 192, 50, 0.5)',
+               data: [2, 123, 154, 76, 54, 23, 5]
+             }
+           ]
+         },
+         radarChartOptions: {
+           responsive: true,
+           maintainAspectRatio: false
+         },
+         doughnutChartData: {
+           labels: ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'],
+           datasets: [
+             {
+               data: [300, 50, 100, 40, 120],
+               backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+               hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774']
+             }
+           ]
+         },
+         doughnutChartOptions: {
+           responsive: true,
+           maintainAspectRatio: false
+         }
+       }
+     }
+   }
+   
+   
+   
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .border-right {
+  }
+   .cascading-admin-card {
+   margin: 100px 0;
+   }
+   .cascading-admin-card .admin-up {
+   margin-left: 4%;
+   margin-right: 4%;
+   margin-top: -20px;
+   }
+   .cascading-admin-card .admin-up .fas,
+   .cascading-admin-card .admin-up .far {
+   box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.2), 0 2px 13px 0 rgba(0, 0, 0, 0.19);
+   padding: 1.7rem;
+   font-size: 2rem;
+   color: #fff;
+   text-align: left;
+   margin-right: 1rem;
+   border-radius: 3px;
+   }
+   .cascading-admin-card .admin-up .data {
+   float: right;
+   margin-top: 2rem;
+   text-align: right;
+   }
+   .admin-up .data p {
+   color: #999999;
+   font-size: 12px;
+   }
+   .classic-admin-card .card-body {
+   color: #fff;
+   margin-bottom: 0;
+   padding: 0.9rem;
+   }
+   .classic-admin-card .card-body p {
+   font-size: 13px;
+   opacity: 0.7;
+   margin-bottom: 0;
+   }
+   .classic-admin-card .card-body h4 {
+   margin-top: 10px;
+   }
+   .md-card-actions.text-center {
+   display: flex;
+   justify-content: center !important;
+   }
+   .contact-form {
+   margin-top: 30px;
+   }
+   .md-has-textarea + .md-layout {
+   margin-top: 15px;
+   }
+   .modal-header {
+   min-height: 16.42857143px;
+   padding: 10px;
+   border-bottom: 1px solid #e5e5e5;
+   }
+   .modal-footer {
+   padding: 10px;
+   text-align: center;
+   border-top: 1px solid #000000;
+   }
+</style>
