b6c6d1930793cb0904b11b9096c3688283662382
DESKTOP-0NM1NV5\User <e0202290@u.nus.edu>
2019-03-25 22:55:42 +0800

    help senpaiiiiiiiiiiii

--- src/views/singleindustry.vue
+++ src/views/singleindustry.vue
@@ -346,10 +346,35 @@
     this.tableData = [];
     this.currname = this.$route.params.indName;
     this.majorname = this.$route.params.currMaj;
-    this.industry = {},
-    this.major = {}
    },
+   watch:{
 
+      '$route' (to, from) {
+    this.tableSuggestedData = [];
+    this.tableData = [];
+    this.currname = this.$route.params.indName;
+    this.majorname = this.$route.params.currMaj;
+      db.ref('major/'+this.majorname+'/industries')
+    .once('value')
+    .then(snapshot=>{
+      this.industry= snapshot.val();
+      this.major = this.industry[this.currname]
+    })
+    .then(()=>{
+        this.updateindustry();
+        this.renderChart();
+    })
+    db.ref('industrymod/'+this.currname)
+      .once('value')
+      .then(snapshot=>{
+        this.suggestedmods = snapshot.val()
+      })
+      .then(()=>{
+        this.recommendmods();
+      })
+    }
+   }
+,
    methods:{
 
     info(item, index, button) {
