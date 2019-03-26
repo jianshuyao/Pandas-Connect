d945b90c04acb9a9a95fce4022815deaf5b7fee8
Joel <e0176286@u.nus.edu>
2019-03-26 20:03:52 +0800

    updated router

--- src/router.js
+++ src/router.js
@@ -1,24 +1,13 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Index from "./views/Index.vue";
 import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Profile from "./views/Profile.vue";
 import MainNavbar from "./layout/MainNavbar.vue";
 import LandingNavbar from "./layout/LandingNavbar.vue";
 import MainFooter from "./layout/MainFooter.vue";
-import testing from "./views/testing.vue"
-import dashboard from "./views/Dashboard.vue"
-import module from "./views/Module.vue"
-import testing2 from "./views/testing2.vue"
-import modlist from "./views/ModsList.vue"
-import modlist2 from "./views/ModsList2.vue"
 import search from "./views/Search.vue"
 import search2 from "./views/search_careers.vue"
-import industry from "./views/Industry.vue"
-import industry2 from "./views/industry2.vue"
-import industry3 from "./views/industry3.vue"
-import major from "./views/Major.vue"
 import top5industries from "./views/top5industries.vue"
 import singleindustry from "./views/singleindustry.vue"
 import company from "./views/company.vue"
@@ -31,7 +20,7 @@ export default new Router({
     {
       path: "/",
       name: "index",
-      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      components: { default: Landing, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
@@ -46,78 +35,7 @@ export default new Router({
         footer: { backgroundColor: "black" }
       }
     },
-    {
-      path: "/login",
-      name: "login",
-      components: { default: Login, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 }
-      }
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/testing",
-      name: "testing",
-      components: { default: testing, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/dashboard",
-      name: "dashboard",
-      components: { default: dashboard, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/module",
-      name: "module",
-      components: { default: module, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/testing2",
-      name: "testing2",
-      components: { default: testing2, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/modlist",
-      name: "modlist",
-      components: { default: modlist, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/modlist2",
-      name: "modlist2",
-      components: { default: modlist2, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    }
-    ,
+    
     {
       path: "/search_careers",
       name: "search_careers",
@@ -131,33 +49,6 @@ export default new Router({
       path: "/search",
       name: "search",
       components: { default: search, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/industry",
-      name: "industry",
-      components: { default: industry2, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/industry3",
-      name: "industry3",
-      components: { default: industry3, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/major",
-      name: "major",
-      components: { default: major, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
