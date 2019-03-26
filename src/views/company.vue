f3330d65c0172b28dc7fa24f7746dab4066180f0
Andrew <andrewthamwj@gmail.com>
2019-03-26 21:31:38 +0800

    Finishing touches

--- src/views/company.vue
+++ src/views/company.vue
@@ -120,7 +120,7 @@
                </mdb-col>
                <mdb-col md="2" lg="6">
                   <mdb-card class="cascading-admin-card">
-                     <mdb-card-header style="background-color: #b3e5fc;" class="card-title">Rate your chances</mdb-card-header>
+                     <mdb-card-header style="background-color: #b3e5fc;" class="card-title">Rate Your Chances</mdb-card-header>
                      <mdb-card-body>
                         <div class="justify-content-center d-flex align-items-stretch">
                            <mdb-col>
