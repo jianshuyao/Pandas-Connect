699f58fb10db9d66d19176cb562c5f59fed2f533
zhengjiesung <sungzhengjie96@gmail.com>
2019-03-31 03:07:48 +0800

    Update single industry

--- src/views/singleindustry.vue
+++ src/views/singleindustry.vue
@@ -59,17 +59,17 @@
           </mdb-col>
           </mdb-row>
         <mdb-row class="justify-content-center">
-        <a href="#hiring_tag"><button class="button button5">Hiring Trend</button></a>
-        <a href="#salary_tag"><button class="button button5">Salary Statistics</button></a>
-        <a href="#company_tag"><button class="button button5">Compare Companies</button></a>
-        <a href="#recommend_tag"><button class="button button5">Recommends</button></a>
+        <a href="#hiring_salary_tag" v-smooth-scroll><button class="button button5">Overview Hiring & Salary</button></a>
+        <a href="#salary_tag" v-smooth-scroll><button class="button button5">Salary Spread</button></a>
+        <a href="#company_tag" v-smooth-scroll><button class="button button5">Compare Companies</button></a>
+        <a href="#recommend_tag" v-smooth-scroll><button class="button button5">Recommends</button></a>
         </mdb-row>
         </mdb-card>
         </mdb-row>
         <a name="myanchor">
     <h1 style="padding-top: 95px; margin-top: -40px;">My anchor</h1>
 </a>
-<a class = "anchor" id="hiring_tag"></a>
+<a class = "anchor" id="hiring_salary_tag"></a>
         <mdb-row class="justify-content-center d-flex align-items-stretch">
           <mdb-col md="1" lg="6">
             <mdb-card class="cascading-admin-card">
@@ -139,7 +139,7 @@
             <mdb-card class="cascading-admin-card">
               <mdb-card-header
                 class="card-title"
-                >Companies</mdb-card-header
+                >Compare Companies</mdb-card-header
               >
               <mdb-card-body>
                 <div v-if="this.loaded" style="display: block">
